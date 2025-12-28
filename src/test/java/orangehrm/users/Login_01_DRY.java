20b53e2aa03d4cfbdd11046f942ac1fb9fc48912
hanhtth <hanhtth3@gmail.com>
2025-12-28 15:28:10 +0700

    Update DRY

---
+++ src/test/java/orangehrm/users/Login_01_DRY.java
@@ -0,0 +1,74 @@
+package orangehrm.users;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Login_01_DRY {
+    private WebDriver driver;
+    @BeforeClass
+    public void beforeClass(){
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+    @Test
+    public void Login_01_Empty(){
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        driver.findElement(By.cssSelector("input[name ='username']")).sendKeys("");
+        driver.findElement(By.cssSelector("input[name ='password']")).sendKeys("");
+        driver.findElement(By.cssSelector("button.orangehrm-login-button")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//input[@name='username']/parent::div/following-sibling::span")).getText(),"Required");
+        Assert.assertEquals(driver.findElement(By.xpath("//input[@name='password']/parent::div/following-sibling::span")).getText(),"Required");
+    }
+    @Test
+    public void Login_01_Invalid_User(){
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        driver.findElement(By.cssSelector("input[name ='username']")).sendKeys("hanhtth3@gmail.com");
+        driver.findElement(By.cssSelector("input[name ='password']")).sendKeys("123456");
+        driver.findElement(By.cssSelector("button.orangehrm-login-button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.oxd-alert-content--error")).getText(),"Invalid credentials");
+    }
+    @Test
+    public void Login_01_Invalid_Password(){
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        driver.findElement(By.cssSelector("input[name ='username']")).sendKeys("Admin");
+        driver.findElement(By.cssSelector("input[name ='password']")).sendKeys("admin123@!#");
+        driver.findElement(By.cssSelector("button.orangehrm-login-button")).click();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.oxd-alert-content--error")).getText(),"Invalid credentials");
+    }
+    @Test
+    public void Login_01_Valid_User_Password(){
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        driver.findElement(By.cssSelector("input[name ='username']")).sendKeys("Admin");
+        driver.findElement(By.cssSelector("input[name ='password']")).sendKeys("admin123");
+        driver.findElement(By.cssSelector("button.orangehrm-login-button")).click();
+
+        Assert.assertTrue(isAllLoadingSpinnerInvisible());
+        Assert.assertEquals(driver.findElement(By.cssSelector("h6.oxd-topbar-header-breadcrumb-module")).getText(),"Dashboard");
+    }
+    public boolean isAllLoadingSpinnerInvisible(){
+        return new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions
+                .invisibilityOfAllElements(driver.findElements(By.cssSelector("div.oxd-loading-spinner"))));
+    }
+    @AfterClass
+    public void  afterClass(){
+        driver.quit();
+    }
+
+}
